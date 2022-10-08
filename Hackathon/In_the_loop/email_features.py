4f243f310bf171b876c689a965c40a0995fab43d
Jatin Tiwari <myemail>
2022-10-08 08:51:42 +0530

    f

--- Hackathon/In_the_loop/email_features.py
+++ Hackathon/In_the_loop/email_features.py
@@ -14,25 +14,25 @@ def receive_emails():
 
 def openAI(title):
 
-    # openai.api_key ="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"
+    openai.api_key ="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"
 
 
-    # response = openai.Completion.create(
-    # model="text-davinci-002",
-    # prompt=f"Email for {title}",
-    # temperature=0.7,
-    # max_tokens=256,
-    # top_p=1,
-    # frequency_penalty=0,
-    # presence_penalty=0
-    # )
-    # print(response['choices'][0]['text']) 
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=f"Email for {title}",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print(response['choices'][0]['text']) 
 
-    # return response['choices'][0]['text']
-    return "OPENAI FUnction is working"
+    return response['choices'][0]['text']
+    # return "OPENAI FUnction is working"
 
 
-def SendEmail(sender_email, receiver_email, body):
+def SendEmail(sender_email, receiver_email, body, title):
 
     ## FILE TO SEND AND ITS PATH
     filename = 'some_file.csv'
@@ -41,17 +41,17 @@ def SendEmail(sender_email, receiver_email, body):
     msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = receiver_email
-    msg['Subject'] = 'Report Update'
-    body = 'Body of the message goes in here'
+    msg['Subject'] = title
+    body = body
     msg.attach(MIMEText(body, 'plain'))
 
     ## ATTACHMENT PART OF THE CODE IS HERE
-    attachment = open(r'C:\xampp\htdocs\website\Hackathons\testing codes\email.py', 'rb')
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    msg.attach(part)
+    # attachment = open(r'C:\xampp\htdocs\website\Hackathons\testing codes\email.py', 'rb')
+    # part = MIMEBase('application', "octet-stream")
+    # part.set_payload((attachment).read())
+    # encoders.encode_base64(part)
+    # part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # msg.attach(part)
 
     try:
         print("Message Sending......")
